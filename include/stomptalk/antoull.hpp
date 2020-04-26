e06256d3e4144f17774a045a315299ea60ee31ab
meil <1@1.1>
2020-04-26 22:25:12 +0300

    antoull+

--- include/stomptalk/antoull.hpp
+++ include/stomptalk/antoull.hpp
@@ -6,30 +6,28 @@
 namespace stomptalk {
 namespace detail {
 
-template<std::size_t N>
+template<class T, std::size_t N>
 struct antoull
 {
-    static constexpr auto pow = 10 * antoull<N - 1>::pow;
-    static inline
-    std::int64_t conv(const char *ptr) noexcept
+    static constexpr auto pow = 10 * antoull<T, N - 1>::pow;
+    static T conv(const char *ptr) noexcept
     {
         auto i = static_cast<std::intptr_t>(*ptr - '0');
         if ((i < 0) || (i > 9))
-            return std::numeric_limits<std::int64_t>::min();
-        return static_cast<std::int64_t>(i * pow) + antoull<N - 1>::conv(++ptr);
+            return std::numeric_limits<T>::min();
+        return static_cast<T>(i * pow) + antoull<T, N - 1>::conv(++ptr);
     }
 };
 
-template<>
-struct antoull<1>
+template<class T>
+struct antoull<T, 1>
 {
     static constexpr auto pow = std::int64_t{1};
-    static inline
-    std::int64_t conv(const char *ptr) noexcept
+    static T conv(const char *ptr) noexcept
     {
         auto i = static_cast<std::intptr_t>(*ptr - '0');
         return ((i < 0) || (i > 9)) ?
-            std::numeric_limits<std::int64_t>::min() : i;
+            std::numeric_limits<T>::min() : static_cast<T>(i);
     }
 };
 
@@ -42,24 +40,24 @@ std::int64_t antoull(const char *ptr, std::size_t n) noexcept
 {
     switch (n)
     {
-    case 0x12: return detail::antoull<0x12>::conv(ptr);
-    case 0x11: return detail::antoull<0x11>::conv(ptr);
-    case 0x10: return detail::antoull<0x10>::conv(ptr);
-    case 0x0f: return detail::antoull<0x0f>::conv(ptr);
-    case 0x0e: return detail::antoull<0x0e>::conv(ptr);
-    case 0x0d: return detail::antoull<0x0d>::conv(ptr);
-    case 0x0c: return detail::antoull<0x0c>::conv(ptr);
-    case 0x0b: return detail::antoull<0x0b>::conv(ptr);
-    case 0x0a: return detail::antoull<0x0a>::conv(ptr);
-    case 0x09: return detail::antoull<0x09>::conv(ptr);
-    case 0x08: return detail::antoull<0x08>::conv(ptr);
-    case 0x07: return detail::antoull<0x07>::conv(ptr);
-    case 0x06: return detail::antoull<0x06>::conv(ptr);
-    case 0x05: return detail::antoull<0x05>::conv(ptr);
-    case 0x04: return detail::antoull<0x04>::conv(ptr);
-    case 0x03: return detail::antoull<0x03>::conv(ptr);
-    case 0x02: return detail::antoull<0x02>::conv(ptr);
-    case 0x01: return detail::antoull<0x01>::conv(ptr);
+    case 0x12: return detail::antoull<std::int64_t, 0x12>::conv(ptr);
+    case 0x11: return detail::antoull<std::int64_t, 0x11>::conv(ptr);
+    case 0x10: return detail::antoull<std::int64_t, 0x10>::conv(ptr);
+    case 0x0f: return detail::antoull<std::int64_t, 0x0f>::conv(ptr);
+    case 0x0e: return detail::antoull<std::int64_t, 0x0e>::conv(ptr);
+    case 0x0d: return detail::antoull<std::int64_t, 0x0d>::conv(ptr);
+    case 0x0c: return detail::antoull<std::int64_t, 0x0c>::conv(ptr);
+    case 0x0b: return detail::antoull<std::int64_t, 0x0b>::conv(ptr);
+    case 0x0a: return detail::antoull<std::int64_t, 0x0a>::conv(ptr);
+    case 0x09: return detail::antoull<std::int64_t, 0x09>::conv(ptr);
+    case 0x08: return detail::antoull<std::int64_t, 0x08>::conv(ptr);
+    case 0x07: return detail::antoull<std::int64_t, 0x07>::conv(ptr);
+    case 0x06: return detail::antoull<std::int64_t, 0x06>::conv(ptr);
+    case 0x05: return detail::antoull<std::int64_t, 0x05>::conv(ptr);
+    case 0x04: return detail::antoull<std::int64_t, 0x04>::conv(ptr);
+    case 0x03: return detail::antoull<std::int64_t, 0x03>::conv(ptr);
+    case 0x02: return detail::antoull<std::int64_t, 0x02>::conv(ptr);
+    case 0x01: return detail::antoull<std::int64_t, 0x01>::conv(ptr);
     case 0x00: return 0;
     default:;
     }
