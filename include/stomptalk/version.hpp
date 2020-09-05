095f0410ba8786a8f3b7149658c73e312deaa953
meil <1>
2020-09-05 12:34:56 +0300

    v1.2.0

---
+++ include/stomptalk/version.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string_view>
+
+namespace stomptalk {
+
+std::string_view version() noexcept;
+
+} // namespace stomptalk
---
+++ src/version.cpp
@@ -0,0 +1,15 @@
+#include "stomptalk/version.hpp"
+
+namespace stomptalk {
+
+std::string_view version() noexcept
+{
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+    constexpr std::string_view rc = STR(STOMPTALK_VERSION);
+#undef STR_HELPER
+#undef STR
+    return rc;
+}
+
+} // namespace stomptalk
