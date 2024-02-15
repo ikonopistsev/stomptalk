a0ababdfaf36ca54d128069a88bd905817733667
meil <1@1.1>
2024-02-15 12:18:42 +0200

    hf

--- include/stomptalk/fnv1a.hpp
+++ include/stomptalk/fnv1a.hpp
@@ -91,7 +91,7 @@ template<class T, fnv1a::type H>
 struct static_hash
 {
     constexpr static auto value = 
-        fnv1a::calc_hash<decltype(T::text)>(T::text.begin(), T::text.end());
+        fnv1a::calc_hash<decltype(T::text)>(std::begin(T::text), std::end(T::text));
     using enable_type = 
         typename std::enable_if<value == H, std::nullptr_t>::type;
 };
