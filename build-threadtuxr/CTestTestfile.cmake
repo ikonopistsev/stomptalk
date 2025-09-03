001cacea201dee8dbf710f53ba2d6e320c3fd652
meil <1@1.1>
2025-09-03 22:04:46 +0300

    fix

--- build-threadtuxr/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/meil/devel/linux/stomptalk-meta/stomptalk
-# Build directory: /home/meil/devel/linux/stomptalk-meta/stomptalk/build-threadtuxr
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(stomptalk_test "/home/meil/devel/linux/stomptalk-meta/stomptalk/build-threadtuxr/stomptalk_test")
-set_tests_properties(stomptalk_test PROPERTIES  _BACKTRACE_TRIPLES "/home/meil/devel/linux/stomptalk-meta/stomptalk/CMakeLists.txt;60;add_test;/home/meil/devel/linux/stomptalk-meta/stomptalk/CMakeLists.txt;0;")
